e3508eeadae1535a814bf4a13d45a44aacdaecf6
apdocentepba-hub <apdocentepba@gmail.com>
2026-03-12 07:50:51 -0300

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ async function postData(data) {
   const res = await fetch(WEB_APP_URL, {
     method: "POST",
     headers: {
-      "Content-Type": "text/plain;charset=utf-8"
+      "Content-Type": "application/json;charset=utf-8"
     },
     body: JSON.stringify(data)
   });
